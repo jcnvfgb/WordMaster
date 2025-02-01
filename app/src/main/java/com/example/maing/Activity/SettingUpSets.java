4dcbfed77a9556c652ce3a9a1159dcc6d8cf94bc
jcnvfgb <lyamin1957@gmail.com>
2025-02-01 08:03:32 +0300

    Setting up the set view form

--- app/src/main/java/com/example/maing/Activity/SettingUpSets.java
+++ app/src/main/java/com/example/maing/Activity/SettingUpSets.java
@@ -1,16 +1,32 @@
 package com.example.maing.Activity;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.maing.Adapter.SetAdapter;
+import com.example.maing.DataBase.DatabaseHelper;
+import com.example.maing.Domain.SetModel;
 import com.example.maing.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class SettingUpSets extends AppCompatActivity {
+    FloatingActionButton floatingId;
+    RecyclerView recyclerView;
+    ArrayList<SetModel> arrayList = new ArrayList<>();
+    DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +37,30 @@ public class SettingUpSets extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        floatingId = findViewById(R.id.floatingId);
+        recyclerView = findViewById(R.id.recyclerView);
+        databaseHelper = new DatabaseHelper(this);
+
+        Cursor cursor = databaseHelper.getAllSets();
+        while (cursor.moveToNext()) {
+            arrayList.add(new SetModel(cursor.getString(1),
+                    cursor.getInt(0),
+                    cursor.getInt(2)));
+        }
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        SetAdapter adapter = new SetAdapter(this, arrayList);
+        recyclerView.setAdapter(adapter);
+
+        floatingId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int id_len = getIntent().getIntExtra("id", 0);
+                Log.e("SettingUpSets", "id_language  " + id_len);
+                //startActivity(new Intent(SettingUpSets.this, MainActivity2.class));
+            }
         });
     }
 }
--- app/src/main/java/com/example/maing/Adapter/SetAdapter.java
+++ app/src/main/java/com/example/maing/Adapter/SetAdapter.java
@@ -1,4 +1,75 @@
 package com.example.maing.Adapter;
 
-public class SetAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.maing.Domain.LanguageModel;
+import com.example.maing.Domain.SetModel;
+import com.example.maing.R;
+
+import java.util.ArrayList;
+
+public class SetAdapter extends RecyclerView.Adapter<SetAdapter.ViewHolder> {
+    Context context;
+    ArrayList<SetModel> arrayList = new ArrayList<>();
+
+    public SetAdapter(Context context, ArrayList<SetModel> arrayList) {
+        this.context = context;
+        this.arrayList = arrayList;
+    }
+
+    @NonNull
+    @Override
+    public SetAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.language_item, parent, false);
+        return new SetAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SetAdapter.ViewHolder holder, int position) {
+        holder.title.setText(arrayList.get(position).getSetName());
+
+        switch (position) {
+            case 0:
+                holder.background_img.setImageResource(R.drawable.bg_1);
+                holder.layout.setBackgroundResource(R.drawable.list_background_1);
+                break;
+            case 1:
+                holder.background_img.setImageResource(R.drawable.bg_2);
+                holder.layout.setBackgroundResource(R.drawable.list_background_1);
+                break;
+            case 2:
+                holder.background_img.setImageResource(R.drawable.bg_3);
+                holder.layout.setBackgroundResource(R.drawable.list_background_1);
+                break;
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title, quantityWord;
+        ImageView background_img;
+        ConstraintLayout layout;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.titleTxt);
+            quantityWord = itemView.findViewById(R.id.quantityWord);
+            background_img = itemView.findViewById(R.id.background_img);
+            layout = itemView.findViewById(R.id.dictionary_layout);
+        }
+    }
 }
