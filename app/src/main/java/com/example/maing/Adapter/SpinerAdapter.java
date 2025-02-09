a3ded1406944d41145f2881191f9fb0c6b4281c7
jcnvfgb <lyamin1957@gmail.com>
2025-02-10 02:30:39 +0300

    Adding functionality for free mode

---
+++ app/src/main/java/com/example/maing/Adapter/SpinerAdapter.java
@@ -0,0 +1,52 @@
+package com.example.maing.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.maing.Domain.SetModel;
+import com.example.maing.R;
+
+import java.util.ArrayList;
+
+public class SpinerAdapter extends BaseAdapter {
+    private Context context;
+    private ArrayList<SetModel> arrayList;
+
+    public SpinerAdapter(Context context, ArrayList<SetModel> arrayList) {
+        this.context = context;
+        this.arrayList = arrayList;
+    }
+
+    @Override
+    public int getCount() {
+        return arrayList != null ? arrayList.size() : 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return i;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View rootView = LayoutInflater.from(context)
+                .inflate(R.layout.spinner_item, viewGroup, false);
+
+        TextView txtName = rootView.findViewById(R.id.titleTxt);
+        TextView desc = rootView.findViewById(R.id.wordForTest);
+
+        txtName.setText(arrayList.get(i).getSetName());
+        desc.setText(arrayList.get(i).getId_set());
+
+        return rootView;
+    }
+}
